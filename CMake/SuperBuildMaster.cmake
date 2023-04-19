3106da0352c11a6f40f560b67b59113fff668569
Thomas Liao <tliao@carboncopies.org>
2023-04-19 01:02:43 +0100

    Setup Initial Repo

--- CMake/SuperBuildMaster.cmake
+++ CMake/SuperBuildMaster.cmake
@@ -13,5 +13,5 @@ set (EXTRA_CMAKE_ARGS)
 # include(${CMAKE_UTILS_DIR}/SuperBuilds/Boost.cmake)
 
 include(${CMAKE_UTILS_DIR}/SuperBuilds/YAMLCPP.cmake)
-include(${CMAKE_UTILS_DIR}/SuperBuilds/BG.cmake)
+include(${CMAKE_UTILS_DIR}/SuperBuilds/API.cmake)
 

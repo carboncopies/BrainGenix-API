867cb80d6f21fd511d3f23b41660e4059527caaa
Thomas Liao <tliao@carboncopies.org>
2023-04-19 01:53:21 +0100

    Fix Version File Not Found In CI

--- CMake/Scripts/VersioningSystem/VersioningSystem.cmake
+++ CMake/Scripts/VersioningSystem/VersioningSystem.cmake
@@ -5,7 +5,7 @@
 BGBuildLogger(${Green} "Configuring Versioning System")
 
 message(" -- Configuring File Version.h.in")
-configure_File(${CMAKE_SCRIPTS_DIR}/VersioningSystem/ERS_Version.h.in ${SRC_DIR}/Interface/Configuration/VersioningSystem/ERS_Version.h)
+configure_File(${CMAKE_SCRIPTS_DIR}/VersioningSystem/Version.h.in ${SRC_DIR}/Interface/Configuration/VersioningSystem/Version.h)
 message(" -- Adding Version Header Target")
 add_subdirectory(${SRC_DIR}/Interface/Configuration/VersioningSystem)
 

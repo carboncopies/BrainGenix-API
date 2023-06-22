c1718850b988a9046589a5025fcb119318f072a3
Thomas Liao <tliao@carboncopies.org>
2023-06-22 15:51:31 -0700

    Cleanup YamlCPP Superbuild Process

--- CMake/SuperBuilds/YAMLCPP.cmake
+++ CMake/SuperBuilds/YAMLCPP.cmake
@@ -28,7 +28,7 @@ if (USE_SUPERBUILD)
 
     message(STATUS "Adding CMake Path Argument")
     list (APPEND EXTRA_CMAKE_ARGS
-      -Dyaml-cpp_DIR=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Install/ThirdParty_${TARGET_NAME}/  
+      -Dyaml-cpp_INSTALL_DIR=${CMAKE_CURRENT_BINARY_DIR}/Dependencies/Install/ThirdParty_${TARGET_NAME}/  
     )
     message(STATUS "Added CMake Path Argument")
 
@@ -36,16 +36,27 @@ if (USE_SUPERBUILD)
 # Run second part of build, create target/find library we built in the above superbuild target
 else()
 
-    option(yaml-cpp_DIR "Location Of yaml-cpp Build (Used In Superbuild Process)")
+    option(yaml-cpp_INSTALL_DIR "Location Of yaml-cpp Build (Used In Superbuild Process)")
 
-    message(STATUS "${TARGET_NAME} Install Directory: ${yaml-cpp_DIR}")
+    message(STATUS "${TARGET_NAME} Install Directory: ${yaml-cpp_INSTALL_DIR}")
 
-    FILE(GLOB_RECURSE Includes ${yaml-cpp_DIR}*/*.h)
-    FILE(GLOB_RECURSE Libs ${yaml-cpp_DIR}*/*.a)
 
-    add_library(yaml-cpp ${Includes})
-    target_link_libraries(yaml-cpp ${Libs})
-    set_target_properties(yaml-cpp PROPERTIES LINKER_LANGUAGE CXX)
-    target_include_directories(yaml-cpp PUBLIC ${yaml-cpp_DIR}/include)
+    # Set configuration for oatpp package to be found using find_package
+    set(yaml-cpp_DIR ${yaml-cpp_INSTALL_DIR}/share/cmake/yaml-cpp)
+
+
+    # Now do the find_package stuff like normal (from the oatpp-swagger github example)
+    find_package(yaml-cpp REQUIRED)
+
+    if(yaml-cpp_FOUND)
+      message(STATUS "Found yaml-cpp version: ${yaml-cpp_VERSION_STRING}")
+    else()
+      message(FATAL_ERROR "Could not find yaml-cpp")
+    endif()
+
+    include_directories(${yaml-cpp_INCLUDE_DIRS})
+
+    # Great, now this can be linked to via oatpp::oatpp, and all headers/libs are taken care of by the above code
+
 
 endif()

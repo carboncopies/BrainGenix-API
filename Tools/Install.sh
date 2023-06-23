0ab9e480d4103de495d7f545ceec84f0af9ae667
Thomas Liao <tliao@carboncopies.org>
2023-06-23 12:46:32 -0700

    Try evpp

--- Tools/Install.sh
+++ Tools/Install.sh
@@ -4,10 +4,10 @@ COMPILER_DEPS="git wget cmake g++"
 # Backward Deps
 BACKWARD_DEPS="binutils-dev libunwind-dev libdwarf-dev libdw-dev"
 
-# Oatpp-Postgresql Dependencies
-OATPP_POSTGRESQL_DEPS="libpostgresql-ocaml-dev"
+# evpp Dependencies
+EVPP_DEPS="libevent-dev libgoogle-glog-dev"
 
 # Install Everything
-INSTALL_COMMAND="sudo apt install $OATPP_POSTGRESQL_DEPS $BACKWARD_DEPS $COMPILER_DEPS -y"
+INSTALL_COMMAND="sudo apt install $EVPP_DEPS $BACKWARD_DEPS $COMPILER_DEPS -y"
 echo "Running Install Command: $INSTALL_COMMAND"
 $INSTALL_COMMAND

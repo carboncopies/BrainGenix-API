607fd988a3ad686b6cad97e21c090c5dbdf75c76
Thomas Liao <tliao@carboncopies.org>
2023-06-23 16:15:48 -0700

    Fix Build System Typo

--- CMake/Scripts/LinuxBundle/LinuxBundle.cmake
+++ CMake/Scripts/LinuxBundle/LinuxBundle.cmake
@@ -2,10 +2,10 @@
 # BG Build Versioning System #
 ###############################
 
-BGBuildLogger(${Cyan} "Configuring Darwin Bundle System")
+BGBuildLogger(${Cyan} "Configuring Linux Bundle System")
 
 add_executable(${PROJECT_NAME} ${MAIN_SOURCES})
 
 set(EXECUTABLE_TYPE "Binary")
 
-BGBuildLogger(${BoldCyan} "Finished Configuring Darwin Bundle System")
+BGBuildLogger(${BoldCyan} "Finished Configuring Linux Bundle System")

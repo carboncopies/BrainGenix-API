2113c43d3454253fdff64ff9d7e75945ef716eb0
Thomas Liao <tliao@carboncopies.org>
2023-06-23 14:17:11 -0700

    Update Install Script To AutoUpdate Submodules

--- Tools/Install.sh
+++ Tools/Install.sh
@@ -12,7 +12,11 @@ INSTALL_COMMAND="sudo apt install $BACKWARD_DEPS $COMPILER_DEPS -y"
 echo "Running Install Command: $INSTALL_COMMAND"
 $INSTALL_COMMAND
 
+# Update Submodules
+echo "Updating Submodules"
+git submodule update --init
+
 # Bootstrap vcpkg
-echo "Setting Up vcpkg, ensure you have the submodules cloned!"
+echo "Setting Up vcpkg"
 ./../ThirdParty/vcpkg/bootstrap-vcpkg.sh
 echo "Done, you can now run Build.sh"

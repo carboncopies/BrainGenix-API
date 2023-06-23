264c15db01c2c355d0d1837e35d3ca3cd121ec48
Thomas Liao <tliao@carboncopies.org>
2023-06-22 17:02:21 -0700

    Add JSON Example

--- Source/Core/Main.h
+++ Source/Core/Main.h
@@ -13,10 +13,14 @@
 #include <iostream>
 
 // Third-Party Libraries (BG convention: use <> instead of "")
+#include <oatpp/web/server/HttpConnectionHandler.hpp>
+
 #include <oatpp/network/Server.hpp>
+#include <oatpp/network/tcp/server/ConnectionProvider.hpp>
+#include <oatpp/parser/json/mapping/ObjectMapper.hpp>
+#include <oatpp/core/macro/codegen.hpp>
 
 // Internal Libraries (BG convention: use <> instead of "")
-
 
 
 

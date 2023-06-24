4e5ad73ead5c2e9be18274438e7c58b1947bd3cd
Thomas Liao <tliao@carboncopies.org>
2023-06-23 17:54:44 -0700

    Update CMake To Copy Config File

--- CMake/Scripts/LinuxBundle/LinuxBundle.cmake
+++ CMake/Scripts/LinuxBundle/LinuxBundle.cmake
@@ -8,4 +8,6 @@ add_executable(${PROJECT_NAME} ${MAIN_SOURCES})
 
 set(EXECUTABLE_TYPE "Binary")
 
+file(COPY "${SRC_DIR}/Data/" DESTINATION "${BIN_DIR}")
+
 BGBuildLogger(${BoldCyan} "Finished Configuring Linux Bundle System")

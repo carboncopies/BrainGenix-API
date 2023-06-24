33ecc3a64c3c555fc4548bd55096652a8f10d039
Thomas Liao <tliao@carboncopies.org>
2023-06-24 16:28:17 -0700

    Remove Old Dependencies

--- ThirdParty/SuperBuild/evpp-0.7.0/cmake/rpm_kernel_release.sh
+++
@@ -1,3 +0,0 @@
-EL=`uname -r | awk -F. '{print $4}'`
-ARCH=`uname -r | awk -F. '{print $5}'`
-echo -n ${EL}.${ARCH}
--- ThirdParty/SuperBuild/oatpp-postgresql-1.3.0/test/oatpp-postgresql/migration/IntTest.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS test_ints;
-
-CREATE TABLE test_ints (
-  f_int16   smallint,
-  f_int32   integer,
-  f_int64   bigint,
-  f_bool    boolean,
-  f_enum    int4
-);

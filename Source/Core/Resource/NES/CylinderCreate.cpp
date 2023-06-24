f08a47276f2ef7eeca0c5e5288e8cbc60388ceaf
Thomas Liao <tliao@carboncopies.org>
2023-06-24 15:59:57 -0700

    Add Cylinde

---
+++ Source/Core/Resource/NES/CylinderCreate.cpp
@@ -0,0 +1,83 @@
+#include <Resource/NES/CylinderCreate.h>
+
+
+namespace BG {
+namespace API {
+namespace Resource {
+
+namespace NES {
+namespace Geometry {
+namespace Shape {
+namespace Cylinder {
+namespace Create {
+
+Route::Route(Server::Server *_Server, restbed::Service &_Service) {
+  Server_ = _Server;
+
+  // Setup List Of Params
+  RequiredParams_.push_back("Point1Radius_nm");
+  RequiredParams_.push_back("Point1Position_nm");
+  RequiredParams_.push_back("Point2Radius_nm");
+  RequiredParams_.push_back("Point2Position_nm");
+
+  OptionalParams_.push_back("Name");
+
+
+  // Setup Callback
+  auto Callback(std::bind(&Route::RouteCallback, this, std::placeholders::_1));
+
+  // Register This Route With Server
+  std::shared_ptr<restbed::Resource> RouteResource = std::make_shared<restbed::Resource>();
+  RouteResource->set_path("/NES/Geometry/Shape/Cylinder/Create");
+  RouteResource->set_method_handler("GET", Callback);
+  _Service.publish(RouteResource);
+
+
+}
+
+Route::~Route() {
+
+}
+
+
+void Route::RouteCallback(const std::shared_ptr<restbed::Session> _Session) {
+    const std::shared_ptr<const restbed::Request> Request = _Session->get_request();
+    Server_->TotalQueries++;
+
+    // Check Parameters, Early Out If Invalid
+    if (!Util::HasRequiredParams(Request.get(), &RequiredParams_)) {
+      Util::SendCode(_Session.get(), 2);
+      return;
+    }
+
+    // Auth Check
+    if (!Util::IsAuthorized(Request.get())) {
+      Util::SendCode(_Session.get(), 4);
+      return;
+    }
+
+    
+
+    // Get Params
+    // float Radius_nm = Request->get_query_parameter("Radius_nm", -1);
+    // std::string Center_nm = Request->get_query_parameter("Center_nm", "");
+    // std::string Name = Request->get_query_parameter("Name", "undefined");
+
+
+    // Build Response And Send
+    nlohmann::json Response;
+    Response["StatusCode"] = 3;
+    Response["ShapeID"] = -1;
+
+    Util::SendJSON(_Session.get(), &Response);
+}
+
+}; // Close Namespace
+}; // Close Namespace
+}; // Close Namespace
+}; // Close Namespace
+}; // Close Namespace
+
+}; // Close Namespace Resource
+}; // Close Namespace API
+}; // Close Namespace BG

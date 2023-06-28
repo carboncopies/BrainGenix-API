c32062218cd99797e2aaa56bc35855957ce83931
Thomas Liao <tliao@carboncopies.org>
2023-06-28 14:55:01 -0700

    Add Simulation Reset Route

--- Source/Core/Resource/NES/SimulationReset.cpp
+++ Source/Core/Resource/NES/SimulationReset.cpp
@@ -51,13 +51,26 @@ void Route::RouteCallback(const std::shared_ptr<restbed::Session> _Session) {
 
     
 
-    // Get Params
-    // std::string Name = Request->get_query_parameter("SimulationID", "");
+    // Get Params, Build Upstream Query
+    nlohmann::json UpstreamQuery;
+    UpstreamQuery["SimulationID"] = Request->get_query_parameter("SimulationID", -1);
+    
+
+    std::string UpstreamResponseStr = "";
+    bool UpstreamStatus = Util::NESQueryJSON(Server_->NESClient, "Simulation/Reset", UpstreamQuery.dump(), &UpstreamResponseStr);
+    if (!UpstreamStatus) {
+      Util::SendCode(_Session.get(), 3);
+      return;
+    }
+    nlohmann::json UpstreamResponse = nlohmann::json::parse(UpstreamResponseStr);
+
 
 
     // Build Response And Send
     nlohmann::json Response;
-    Response["StatusCode"] = 3;
+    Response["StatusCode"] = 0;
+
+    std::cout<<"Resetting Simulation With ID "<<Request->get_query_parameter("SimulationID", -1)<<std::endl;
 
     Util::SendJSON(_Session.get(), &Response);
 }

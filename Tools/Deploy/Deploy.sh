9cc6a83a4fd1c4942ece780d24cd719b6a5b00c6
Thomas Liao <tliao@carboncopies.org>
2023-07-16 01:09:36 -0700

    Update Deploy Job

--- Tools/Deploy/Deploy.sh
+++ Tools/Deploy/Deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-echo "Downloading Latest BrainGenix API Package"
-wget -O Artifacts.zip https://gitlab.braingenix.org/api/v4/projects/BrainGenix-API/jobs/artifacts/master/download?job=Ubuntu_2204_Package
+# echo "Downloading Latest BrainGenix API Package"
+# wget -O Artifacts.zip https://gitlab.braingenix.org/api/v4/projects/BrainGenix-API/jobs/artifacts/master/download?job=Ubuntu_2204_Package
 
-echo "Unzipping Artifacts"
+# echo "Unzipping Artifacts"
+
+echo "Finding Current Package Version"
+ls Artifacts/
+cd $Artifacts && PackageVar=$(find -type f -name 'BrainGenix-API*.*.deb');
+echo "Found API Package At $PackageVar"
 
 echo "Uninstalling Current Package"
+sudo apt remove braingenix-api
 
 echo "Installing New Package"
+sudo apt install $PackageVar

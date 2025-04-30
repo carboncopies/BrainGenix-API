ef56f367c4c77cc3dc54f0f2f4a34b9fac08b03a
Thomas Liao <tliao@carboncopies.org>
2025-04-30 12:56:07 -0700

    Cleanup old API Routes

--- Source/Core/Server/Endpoints.cpp
+++ Source/Core/Server/Endpoints.cpp
@@ -27,55 +27,6 @@ void EndpointManager::AddRoutes(restbed::Service &_Service, Server &_Server) {
 
     Auth_GetToken_ = std::make_shared<Resource::Auth::GetToken::Route>(&_Server, _Service);
 
-
-    NES_Geometry_Shape_Sphere_Create_ = std::make_shared<Resource::NES::Geometry::Shape::Sphere::Create::Route>(&_Server, _Service);
-    NES_Geometry_Shape_Sphere_BulkCreate_ = std::make_shared<Resource::NES::Geometry::Shape::Sphere::BulkCreate::Route>(&_Server, _Service);
-    NES_Geometry_Shape_Cylinder_Create_ = std::make_shared<Resource::NES::Geometry::Shape::Cylinder::Create::Route>(Logger_, &_Server, _Service);
-    NES_Geometry_Shape_Cylinder_BulkCreate_ = std::make_shared<Resource::NES::Geometry::Shape::Cylinder::BulkCreate::Route>(&_Server, _Service);
-    NES_Geometry_Shape_Box_Create_ = std::make_shared<Resource::NES::Geometry::Shape::Box::Create::Route>(Logger_, &_Server, _Service);
-    NES_Geometry_Shape_Box_BulkCreate_ = std::make_shared<Resource::NES::Geometry::Shape::Box::BulkCreate::Route>(&_Server, _Service);
-
-    NES_Compartment_BS_Create_ = std::make_shared<Resource::NES::Compartment::BS::Create::Route>(Logger_, &_Server, _Service);
-    NES_Compartment_BS_BulkCreate_ = std::make_shared<Resource::NES::Compartment::BS::BulkCreate::Route>(Logger_, &_Server, _Service);
-
-
-    NES_Connection_Staple_Create_ = std::make_shared<Resource::NES::Connection::Staple::Create::Route>(&_Server, _Service);
-    NES_Connection_Receptor_Create_ = std::make_shared<Resource::NES::Connection::Receptor::Create::Route>(Logger_, &_Server, _Service);
-
-    NES_Neuron_BSNeuron_Create = std::make_shared<Resource::NES::Neuron::BSNeuron::Create::Route>(Logger_, &_Server, _Service);
-
-    NES_Tool_PatchClampDAC_Create_ = std::make_shared<Resource::NES::Tool::PatchClampDAC::Create::Route>(Logger_, &_Server, _Service);
-    NES_Tool_PatchClampDAC_SetOutputList_ = std::make_shared<Resource::NES::Tool::PatchClampDAC::SetOutputList::Route>(Logger_, &_Server, _Service);
-
-    NES_Tool_PatchClampADC_Create_ = std::make_shared<Resource::NES::Tool::PatchClampADC::Create::Route>(Logger_, &_Server, _Service);
-    NES_Tool_PatchClampADC_SetSampleRate_ = std::make_shared<Resource::NES::Tool::PatchClampADC::SetSampleRate::Route>(Logger_, &_Server, _Service);
-    NES_Tool_PatchClampADC_GetRecordedData_ = std::make_shared<Resource::NES::Tool::PatchClampADC::GetRecordedData::Route>(Logger_, &_Server, _Service);
-    
-    NES_Simulation_Create_       = std::make_shared<Resource::NES::Simulation::Create::Route>(Logger_, &_Server, _Service);
-    NES_Simulation_Reset_        = std::make_shared<Resource::NES::Simulation::Reset::Route>(&_Server, _Service);
-    NES_Simulation_GetStatus_    = std::make_shared<Resource::NES::Simulation::GetStatus::Route>(&_Server, _Service);
-    NES_Simulation_RunFor_       = std::make_shared<Resource::NES::Simulation::RunFor::Route>(&_Server, _Service);
-    NES_Simulation_RecordAll_    = std::make_shared<Resource::NES::Simulation::RecordAll::Route>(&_Server, _Service);
-    NES_Simulation_GetRecording_ = std::make_shared<Resource::NES::Simulation::GetRecording::Route>(Logger_, &_Server, _Service);
-    NES_Simulation_BuildMesh_    = std::make_shared<Resource::NES::Simulation::BuildMesh::Route>(Logger_, &_Server, _Service);
-
-    NES_VSDA_EM_Initialize_ = std::make_shared<Resource::NES::VSDA::EM::Initialize::Route>(&_Server, _Service);
-    NES_VSDA_EM_SetupMicroscope_ = std::make_shared<Resource::NES::VSDA::EM::SetupMicroscope::Route>(&_Server, _Service);
-
-    NES_VSDA_EM_DefineScanRegion_ = std::make_shared<Resource::NES::VSDA::EM::DefineScanRegion::Route>(&_Server, _Service);
-    NES_VSDA_EM_QueueRenderOperation_ = std::make_shared<Resource::NES::VSDA::EM::QueueRenderOperation::Route>(&_Server, _Service);
-    NES_VSDA_EM_GetImageStack_ = std::make_shared<Resource::NES::VSDA::EM::GetImageStack::Route>(&_Server, _Service);
-    NES_VSDA_EM_GetImage_ = std::make_shared<Resource::NES::VSDA::EM::GetImage::Route>(&_Server, _Service);
-    NES_VSDA_EM_GetRenderStatus_ = std::make_shared<Resource::NES::VSDA::EM::GetRenderStatus::Route>(&_Server, _Service);
-
-    NES_VSDA_Calcium_DefineScanRegion_ = std::make_shared<Resource::NES::VSDA::Calcium::DefineScanRegion::Route>(&_Server, _Service);
-    NES_VSDA_Calcium_QueueRenderOperation_ = std::make_shared<Resource::NES::VSDA::Calcium::QueueRenderOperation::Route>(&_Server, _Service);
-    NES_VSDA_Calcium_GetImageStack_ = std::make_shared<Resource::NES::VSDA::Calcium::GetImageStack::Route>(&_Server, _Service);
-    NES_VSDA_Calcium_GetImage_ = std::make_shared<Resource::NES::VSDA::Calcium::GetImage::Route>(&_Server, _Service);
-    NES_VSDA_Calcium_GetRenderStatus_ = std::make_shared<Resource::NES::VSDA::Calcium::GetRenderStatus::Route>(&_Server, _Service);
-    NES_VSDA_Calcium_CreateIndicator_ = std::make_shared<Resource::NES::VSDA::Calcium::CreateIndicator::Route>(Logger_, &_Server, _Service);
-    NES_VSDA_Calcium_Setup_ = std::make_shared<Resource::NES::VSDA::Calcium::Setup::Route>(&_Server, _Service);
-
     NES_ = std::make_shared<Resource::NES::Route>(Logger_, &_Server, _Service);
     EVM_ = std::make_shared<Resource::EVM::Route>(Logger_, &_Server, _Service);
 

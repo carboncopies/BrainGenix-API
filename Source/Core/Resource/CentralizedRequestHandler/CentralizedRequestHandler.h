06a5bfead272868f08a2959b8f8e18fe1d089f94
Albert Huynh <ahuynh@carboncopies.org>
2025-12-16 19:51:07 -0800

    started the centralized request handler in the API, header file done

---
+++ Source/Core/Resource/CentralizedRequestHandler/CentralizedRequestHandler.h
@@ -0,0 +1,64 @@
+//==============================================================//
+// This file is part of the BrainGenix-API Unified API Endpoint //
+//==============================================================//
+
+/*
+    Description: This file provides code used in the API routes.
+    Additional Notes: None
+    Date Created: 2025-12-16
+*/
+
+#pragma once    
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <Server/Server.h>
+#include <BG/Common/Logger/Logger.h>
+
+class CentralizedRequestHandler {
+    private:
+        /**
+            * @brief Routes a request from one backend service to another through the API.
+            *
+            * This method allows backend services (NES, VSDA, EVM, ERS) to communicate with
+            * each other by routing requests through the API hub. The API forwards the request
+            * to the target service and returns the response.
+            * Supported Services: NES, VSDA, EVM, ERS
+            *
+            * @param _Server Pointer to the Server instance containing client connections.
+            * @param _Logger Pointer to the logging system instance.
+            * @param _TargetService Name of the API that we want to route the request to.
+            * @param _RPCQuery Name of the RPC query to route the request to.
+            * @param _QueryContent JSON query content to be sent to the API.
+            * @return std::string JSON response string from the target service, or error JSON if routing fails.
+            */
+        std::string RouteToBackendService(Server* _Server, BG::Common::Logger::LoggingSystem* _Logger, std::string _TargetService, std::string _RPCQuery, std::string _QueryContent);
+
+        /**
+            * @brief This function handles the intended functionality of a Request coming from NES
+            *
+            * This method indicates how to handle requests that are coming from NES, how they should be processed etc.
+            *
+            * @param _QueryContent The JSON query content which will be processed and then sent back
+            */
+        std::string handleRequestNES(std::string _QueryContent);
+
+        /**
+            * @brief This function handles the intended functionality of a Request coming from VSDA
+            * This method indicates how to handle requests that are coming from VSDA, how they should be processed etc.
+            *
+            * @param _QueryContent The JSON query content which will be processed and then sent back
+            */
+        std::string handleRequestVSDA(std::string _QueryContent);
+
+        /**
+            * @brief This function handles the intended functionality of a Request coming from EVM
+            * This method indicates how to handle requests that are coming from EVM, how they should be processed etc.
+            *
+            * @param _QueryContent The JSON query content which will be processed and then sent back
+            */
+        std::string handleRequestEVM(std::string _QueryContent);
+}
+
---
+++ Source/Core/Resource/VSDA/VSDA.h
@@ -0,0 +1,76 @@
+#pragma once
+
+// Standard Libraries (BG convention: use <> instead of "")
+#include <string>
+#include <iostream>
+#include <functional>
+
+// Third-Party Libraries (BG convention: use <> instead of "")
+#include <restbed>
+
+#include <nlohmann/json.hpp>
+
+// Internal Libraries (BG convention: use <> instead of "")
+#include <Server/Server.h>
+#include <BG/Common/Logger/Logger.h>
+#include <Util/ParamCheck.h>
+#include <Util/RestbedHelpers.h>
+#include <Util/RPCHelpers.h>
+
+
+namespace BG {
+namespace API {
+namespace Resource {
+
+namespace VSDA {
+
+
+/**
+ * @brief Provides the code to handle route for the API.
+ * 
+ * @param _Server 
+ * @param _Session 
+ */
+class Route {
+    
+private:
+
+    Server::Server *Server_; /**Copy of the server struct*/
+    BG::Common::Logger::LoggingSystem* Logger_; /*Pointer to instance of Logging System*/
+
+
+    void PostHandler(const std::shared_ptr<restbed::Session> _Session, const restbed::Bytes& _Line);
+
+public:
+
+    /**
+     * @brief Construct a new route object
+     * This function registers the routecallback method as a route for the path specified in the descriptor for this class.
+     * 
+     * @param _Logger Pointer to the logging system instance.
+     * @param _Server Pointer to the server struct.
+     * @param _Service Reference to the Restbed service.
+     */
+    Route(BG::Common::Logger::LoggingSystem* _Logger, Server::Server *_Server, restbed::Service &_Service);
+
+    /**
+     * @brief Destroy the Route object
+     * 
+     */
+    ~Route();
+
+
+    /**
+     * @brief This is the route callback used.
+     * 
+     */
+    void RouteCallback(const std::shared_ptr<restbed::Session> _Session);
+
+};
+
+
+}; // Close Namespace
+
+}; // Close Namespace Resource
+}; // Close Namespace API
+}; // Close Namespace BG
